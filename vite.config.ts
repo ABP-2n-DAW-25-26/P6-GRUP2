63641df04f37f996ec2af8bf1b16e45402d14f1c
brandonalcantara17 <brandonalcantara@proton.me>
2026-05-20 17:21:03 +0200

    Added vitest

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -25,4 +26,14 @@ export default defineConfig({
             formVariants: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
+
+    test: {
+        environment: 'jsdom',
+        globals: true,
+    },
 });
